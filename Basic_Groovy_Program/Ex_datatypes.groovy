26653e903a5a2dc5cc5eda3ec3b951a005e3654b
Kundan Kumar <89348788+mindexpert7546@users.noreply.github.com>
2023-10-16 16:41:12 +0530

    Datatypes added in basic folder
    I have add the data types in basic code folder in repo

---
+++ Basic_Groovy_Program/Ex_datatypes.groovy
@@ -0,0 +1,28 @@
+class Ex_datatypes{
+    public static main(String[] args){
+        //it is eg of an int datatype 
+      int a = 15; 
+		
+      //it is eg of a long datatype 
+      long b = 1000L; 
+		
+      //it is eg of a floating point datatype 
+      float c = 13.78f; 
+		
+      //it is eg of a double datatype 
+      double d = 17.5e40; 
+		
+      //it is eg of a BigInteger datatype 
+      BigInteger bigi = 30g; 
+		
+      //it is eg of a BigDecimal datatype 
+      BigDecimal bigd = 13.8g; 
+		
+      println(a); 
+      println(b); 
+      println(c); 
+      println(d); 
+      println(bigi); 
+      println(bigd); 
+    }
+}
