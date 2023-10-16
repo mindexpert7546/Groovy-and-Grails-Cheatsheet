5064da49e61baf062345962227da8b8d4012ce46
Kundan Kumar <89348788+mindexpert7546@users.noreply.github.com>
2023-10-16 15:59:24 +0530

    Semicolon class created for full exp of basic code

---
+++ Basic_Groovy_Program/Semicolon.groovy
@@ -0,0 +1,17 @@
+class Semicolon {
+
+   static void main(String[] args) {
+
+      //def x = data type?
+
+      //def is recommended over any specific data type: so that we can perform
+
+      //activity as per our choice
+
+      def x = 5// def is recommended
+
+      println('Hello World'); //semicolon is optional
+
+   }
+
+}
