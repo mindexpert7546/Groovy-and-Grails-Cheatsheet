0b4c0cbff4d13fc7ee583c4ec441059824fc295e
Kundan Kumar <89348788+mindexpert7546@users.noreply.github.com>
2023-10-17 14:33:06 +0530

    Corrected the comment

--- Basic_Groovy_Program/LargestNo_Array.groovy
+++ Basic_Groovy_Program/LargestNo_Array.groovy
@@ -1,6 +1,6 @@
 class LargestNo_Array{
     public static void main(String[] args){
-        //To create the array
+        //creating an array defination
         int[] arr = [1,6,3,7,4,9,23,4,65,98];
 
         //logic for find largest number from the array 
